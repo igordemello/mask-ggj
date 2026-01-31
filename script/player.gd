f8b90e6b6c9925d50bc17bfdc859961c4ccab4e8
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-01-30 21:49:55 -0300

    movimentacao inicial
    wedefwfw

---
+++ script/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const ACELL = 2.0
+
+var input: Vector2
+
+func get_input():
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return input.normalized()
+
+func _pyshics_process(delta):
+	var playerInput = get_input() 
+	
+	velocity = lerp(velocity, playerInput * SPEED, delta * ACELL)
+	
+	move_and_slide()
+	
+	
+	
+	
